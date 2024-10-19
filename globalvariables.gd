d832f48b7baf0b4b78c47bed6810e55c1b88b1fc
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-19 14:55:23 -0500

    Good art

--- globalvariables.gd
+++ globalvariables.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var difficulty = "hell"
+var difficulty = "Easy"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
