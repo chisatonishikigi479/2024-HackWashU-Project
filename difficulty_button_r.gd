eeb856c630b076ffd8dc000530cc0b796a3133fd
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-19 14:17:27 -0500

    Enemy

---
+++ difficulty_button_r.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+
+func _ready() -> void:
+	if texture_normal:
+		# Get the image from the texture normal
+		var image = texture_normal.get_image()
+		# Create the BitMap
+		var bitmap = BitMap.new()
+		# Fill it from the image alpha
+		bitmap.create_from_image_alpha(image)
+		# Assign it to the mask
+		texture_click_mask = bitmap
