f704874a37ca55344d7c5a56a0ce698181d3e23e
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-19 15:26:37 -0500

    Difficulty buttons implementation

--- title_screen.gd
+++ title_screen.gd
@@ -12,6 +12,28 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://maze_generation_manager.tscn")
 
-
 func _on_difficulty_button_r_pressed() -> void:
-	pass
+	if Globalvariables.difficulty == "Extreme":
+		Globalvariables.difficulty = "Easy"
+	else:
+		if Globalvariables.difficulty == "Hard":
+			Globalvariables.difficulty = "Extreme"
+		if Globalvariables.difficulty == "Medium":
+			Globalvariables.difficulty = "Hard"
+		if Globalvariables.difficulty == "Easy":
+			Globalvariables.difficulty = "Medium"
+	get_node("MarginContainer/DifficultyLabel").text = str("Difficulty: " + Globalvariables.difficulty)
+
+	
+
+func _on_difficulty_button_l_pressed() -> void:
+	if Globalvariables.difficulty == "Easy":
+		Globalvariables.difficulty = "Extreme"
+	else:
+		if Globalvariables.difficulty == "Medium":
+			Globalvariables.difficulty = "Easy"
+		if Globalvariables.difficulty == "Hard":
+			Globalvariables.difficulty = "Medium"
+		if Globalvariables.difficulty == "Extreme":
+			Globalvariables.difficulty = "Hard"
+	get_node("MarginContainer/DifficultyLabel").text = str("Difficulty: " + Globalvariables.difficulty)
