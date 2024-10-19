b9eb72ddacccb8afe86c91b6f3698ef92453af34
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-19 13:47:23 -0500

    Title Screen Functional

--- title_screen.gd
+++ title_screen.gd
@@ -1,6 +1,5 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +8,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://maze_generation_manager.tscn")
