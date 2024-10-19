a7306de66d344cbe2ba1daf8427a7549a50e8af3
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-19 15:48:46 -0500

    made difficulty control size of maze

--- title_screen.gd
+++ title_screen.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var pressed = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,7 +11,9 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://maze_generation_manager.tscn")
+	pressed +=1 #prevents multiple generations
+	if pressed ==1:
+		get_tree().change_scene_to_file("res://maze_generation_manager.tscn")
 
 func _on_difficulty_button_r_pressed() -> void:
 	if Globalvariables.difficulty == "Extreme":
