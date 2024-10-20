0b3e5a28101a5f7f7959d1edeea7343255c3c0e5
chendexter123 <122926433+chendexter123@users.noreply.github.com>
2024-10-20 11:50:39 -0500

    fix gid

--- CatGod.gd
+++ CatGod.gd
@@ -8,8 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if not isHappy:
-		$AnimatedSprite2D.play("angry")
-	else:
-		$AnimatedSprite2D.play("happy")
 	pass
