529e329cf362488f4d64bfe81ed22e34c57369c3
Luke Zhang <randomaopstroller3@gmail.com>
2024-10-19 20:40:05 -0500

    asdfasdf
    asdfasdf

---
+++ CatCharacter.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	
+	if not is_on_floor():
+		# play jump animation!
+		velocity.y += gravity * delta
+		
+	else:
+		#play walk/run animation!
+		pass
+	
+	$AnimatedSprite2D.play("default") #filler animation code
+
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	velocity.x = SPEED
+	move_and_slide()
